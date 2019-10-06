18552ef67808af8f5bac83d5ad349c2d99ba5630
jsalig00 <jsalig00@estudiantes.unileon.es>
2019-10-06 18:08:02 +0200

    0.2 Avances en el planteamiento de funciones

--- src/RootMatrix.java
+++ src/RootMatrix.java
@@ -5,65 +5,123 @@ public class RootMatrix {
 	Scanner scanner = new Scanner(System.in);
 	
 	int matrix[][];
+	int rootMatrix[][];
 	int size;
-	int aux1 = 0;
-	int aux2 = 0;
+	int column = 0;
+	int row = 0;
 	
+	
+	/**
+	 * Constructor de la clase que crea una matriz cuadrada de numeros enteros
+	 * @param size orden de la matriz cuadrada
+	 */
 	public RootMatrix(int size) {
 		this.matrix = new int[size][size];
+		this.rootMatrix = new int[size][size];
 		this.size = size;
-	}
+	}	
 	
-	public void ask4Values() throws InterruptedException {		
-		if(this.aux1 < this.size && this.aux2 < this.size) {
-			System.out.println("\nValor: ");
-			this.matrix[aux2][aux1] = this.scanner.nextInt();
-			this.aux1++;
+	/*
+	 * Metodo que usa el scanner para preguntar y asignar los valores de cada posicion de la matriz
+	*/ 
+	public void ask4Values() throws InterruptedException {	
+		
+		if(this.row < this.size && this.column < this.size) {
+			System.out.print("Fila " +(this.row + 1) + " ");
+		}
+		
+		if(this.column < this.size && this.row < this.size) {
+			System.out.println("Valor " +(column+1)+ ": ");
+			this.matrix[row][column] = this.scanner.nextInt();
+			this.column++;
 			//Realizamos recurrencia para que se llenen todos las columnas de la primera fila en este if
 			ask4Values();		
 		}
-		else if(this.aux2 < this.size) {
+		else if(this.row < this.size) {
 			//Aumentamos la posicion de la fila en uno y reseteamos la de las columnas
-			this.aux2++;
-			this.aux1 = 0;
+			this.row++;
+			this.column = 0;
 			ask4Values();
 		}
 		
-		else if(this.aux2 == this.size) {
+		else if(this.row == this.size) {
 			//Cerramos el scanner ya que en lo que queda de ejecucion del programa no se necesitara leer mas valores por el teclado 
 			//Reseteamos los valores auxiliares que actuan como indice de columnas y filas
-			this.aux1 = 0;
-			this.aux2 = 0;
+			this.column = 0;
+			this.row = 0;
 			scanner.close();
 			
 			Thread.sleep(500);
 			
 			System.out.println("Matriz introducida: \n");
-			printMatrix();
+			printMatrix(this.matrix);
 		}		 
 	}
-
-	public void printMatrix() {		
-		if(this.aux1 < this.size && this.aux2 < this.size) {
-			System.out.print(" "+this.matrix[aux2][aux1]+" ");			
-			this.aux1++;
-			//Realizamos recurrencia para imprimir todas las columnas de la primera fila
-			printMatrix();		
-		}
-		else if(this.aux2 < this.size) {
-			//Aumentamos la posicion de la fila en uno y reseteamos la de las columnas
-			this.aux2++;
-			this.aux1 = 0;
-			System.out.println("\n");
-			printMatrix();
+	
+	/*
+	 * Metodo que se encarga de recorrer todas las posiciones de rootMatrix y de asignarle los valores que devuelve la funcion getRoot
+	 */
+	public void getNumber() {		
+		if(this.column < this.size && this.row < this.size) {			
+			this.rootMatrix[row][column] = getRoot(this.matrix[row][column]);
+			this.column++;
+			//Realizamos recurrencia para que se recorran todos las columnas de la primera fila en este if
+					
 		}
 		
-		if(this.aux1 == this.size && this.aux2 == this.size) {
+		else if(this.row < this.size) {
+			//Aumentamos la posicion de la fila en uno y reseteamos la de las columnas
+			this.row++;
+			this.column = 0;		
+		}
+		
+		else if(this.row == this.size) {
 			//Cerramos el scanner ya que en lo que queda de ejecucion del programa no se necesitara leer mas valores por el teclado 
 			//Reseteamos los valores auxiliares que actuan como indice de columnas y filas
-			this.aux1 = 0;
-			this.aux2 = 0;
+			this.column = 0;
+			this.row = 0;			
+			
+			System.out.println("Matriz de raices cuadradas de la introducida: \n");
+			printMatrix(this.rootMatrix);
+		}
+	}
+	
+	/*
+	 * Metodo que se encarga de calcular la aproximacion entera de la raiz de un numero
+	 */
+	public int getRoot(int value) {
+		
+		
+		
+		
+		return value;
+	}
+	
+	/*
+	 * Metodo que se encarga de previsualizar la matriz por pantalla que se le pase como parametro
+	 */
+	public void printMatrix(int matrix[][]) {		
+		if(this.column < this.size && this.row < this.size) {
+			System.out.print(" "+matrix[this.row][this.column]+" ");			
+			this.column++;
+			//Realizamos recurrencia para imprimir todas las columnas de la primera fila
+			printMatrix(matrix);		
+		}
+		else if(this.row < this.size) {
+			//Aumentamos la posicion de la fila en uno y reseteamos la de las columnas
+			this.row++;
+			this.column = 0;
+			System.out.println("\n");
+			printMatrix(matrix);
+		}
+		
+		if(this.column == this.size && this.row == this.size) {			
+			//Reseteamos los valores auxiliares que actuan como indice de columnas y filas
+			this.column = 0;
+			this.row = 0;
 		}		 
 	}
+	
+	
 
 }
